3ca08d86c963b32416f3ff93df746fc6cf0c5114
Raffael Thome <thome.raffael@gmail.com>
2012-10-03 14:50:33 +0200

    Move things around, add test project

---
+++ SharpNoise.Tests/MapTests.cs
@@ -0,0 +1,81 @@
+﻿using NUnit.Framework;
+
+using SharpNoise.Builders;
+using SharpNoise.Modules;
+using SharpNoise.Utilities;
+
+namespace SharpNoise.Tests
+{
+    [TestFixture]
+    class MapTests
+    {
+        NoiseMap map;
+
+        [SetUp]
+        public void SetUp()
+        {
+            map = new NoiseMap(2, 2);
+        }
+
+        [Test]
+        public void GetSetValueTest()
+        {
+            var expected = 99f;
+            map.SetValue(1, 1, expected);
+
+            var actual = map.GetValue(1, 1);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void BorderValueTest()
+        {
+            var expected = 1f;
+            map.BorderValue = expected;
+            map.Clear(10f);
+
+            Assert.AreEqual(expected, map.GetValue(-1, -1));
+            Assert.AreEqual(expected, map.GetValue(-1, 0));
+            Assert.AreEqual(expected, map.GetValue(-1, 1));
+            Assert.AreEqual(expected, map.GetValue(-1, 2));
+
+            Assert.AreEqual(expected, map.GetValue(2, -1));
+            Assert.AreEqual(expected, map.GetValue(2, 0));
+            Assert.AreEqual(expected, map.GetValue(2, 1));
+            Assert.AreEqual(expected, map.GetValue(2, 2));
+        }
+
+        [Test]
+        public void ClearTest()
+        {
+            var expected = 5f;
+            map.Clear(expected);
+            foreach (var line in map.GetLineReaders())
+                foreach (var value in line)
+                    Assert.AreEqual(expected, value);
+        }
+
+        [Test]
+        public void LineReaderTest()
+        {
+            var builder = new CylinderNoiseMapBuilder();
+            builder.DestNoiseMap = new NoiseMap();
+            builder.SourceModule = new Perlin();
+            builder.SetBounds(0, 180, 0, 5);
+            builder.SetDestSize(12, 6);
+            builder.Build();
+
+            int row = 0, x = 0;
+            foreach (var line in builder.DestNoiseMap.GetLineReaders())
+            {
+                foreach (var value in line)
+                {
+                    Assert.AreEqual(builder.DestNoiseMap.GetValue(x, row), value);
+                    x++;
+                }
+                row++;
+                x = 0;
+            }
+        }
+    }
+}
---
+++ SharpNoise.Tests/NoiseMathTests.cs
@@ -0,0 +1,100 @@
+﻿using NUnit.Framework;
+
+namespace SharpNoise.Tests
+{
+    [TestFixture]
+    class NoiseMathTests
+    {
+        [TestCase]
+        public void ClampTest1()
+        {
+            var expected = 10;
+            Assert.AreEqual(expected, NoiseMath.Clamp(15, 0, expected));
+        }
+
+        [TestCase]
+        public void ClampTest2()
+        {
+            var expected = 5;
+            Assert.AreEqual(expected, NoiseMath.Clamp(expected, 0, 10));
+        }
+
+        [TestCase]
+        public void ClampTest3()
+        {
+            var expected = 10;
+            Assert.AreEqual(expected, NoiseMath.Clamp(5, expected, 20));
+        }
+
+        [TestCase]
+        public void MaxTest()
+        {
+            var min = 0D;
+            var max = 1D; 
+            Assert.AreEqual(max, NoiseMath.Max(min, max));
+        }
+
+        [TestCase]
+        public void MaxTest2()
+        {
+            var min = 0D;
+            var max = 1D;
+            Assert.AreEqual(max, NoiseMath.Max(max, min));
+        }
+
+        [TestCase]
+        public void MinTest()
+        {
+            var min = 0D;
+            var max = 1D;
+            Assert.AreEqual(min, NoiseMath.Min(min, max));
+        }
+
+        [TestCase]
+        public void MinTest2()
+        {
+            var min = 0D;
+            var max = 1D;
+            Assert.AreEqual(min, NoiseMath.Min(max, min));
+        }
+
+        [TestCase]
+        public void LinearTest1()
+        {
+            var value1 = 0D;
+            var value2 = 1D;
+            var alpha = 0.5D;
+
+            var expected = 0.5D;
+            var actual = NoiseMath.Linear(value1, value2, alpha);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase]
+        public void LinearTest2()
+        {
+            var value1 = 0D;
+            var value2 = 1D;
+            var alpha = 0D;
+
+            var expected = 0D;
+            var actual = NoiseMath.Linear(value1, value2, alpha);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase]
+        public void LinearTest3()
+        {
+            var value1 = 0D;
+            var value2 = 5D;
+            var alpha = 1D;
+
+            var expected = 5D;
+            var actual = NoiseMath.Linear(value1, value2, alpha);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
