ea48cff0758d129ad9702b143854d63ff257ddae
Raffael Thome <thome.raffael@gmail.com>
2013-02-18 11:45:43 +0100

    Implement most of the Xml serializer.

---
+++ SharpNoise.Serialization.Tests/XmlSerializerTests.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SharpNoise.Modules;
+using System.IO;
+
+namespace SharpNoise.Serialization.Tests
+{
+    [TestClass]
+    public class XmlSerializerTests
+    {
+        XmlSerializer serializer;
+
+        Add root;
+        Constant source0, source1;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            serializer = new XmlSerializer();
+
+            source0 = new Constant { Value = 1 };
+            source1 = new Constant { Value = 2 };
+            root = new Add { Source0 = source0, Source1 = source1 };
+        }
+
+        [TestMethod]
+        public void Serialization_Xml_Restore_ValuesEqual_Test()
+        {
+            Add restoredModule;
+
+            using (var ms = new MemoryStream())
+            {
+                serializer.Save(root, ms);
+
+                restoredModule = serializer.Restore<Add>(ms);
+            }
+
+            Assert.AreEqual(root.GetValue(0, 0, 0), restoredModule.GetValue(0, 0, 0));
+            Assert.AreEqual(root.Source0.GetValue(0, 0, 0), restoredModule.Source0.GetValue(0, 0, 0));
+            Assert.AreEqual(root.Source1.GetValue(0, 0, 0), restoredModule.Source1.GetValue(0, 0, 0));
+        }
+
+        [TestMethod]
+        public void Serialization_Xml_Restore_TypesEqual_Test()
+        {
+            Module restoredModule;
+
+            using (var ms = new MemoryStream())
+            {
+                serializer.Save(root, ms);
+
+                restoredModule = serializer.Restore(ms);
+            }
+
+            Assert.IsInstanceOfType(restoredModule, root.GetType());
+            Assert.IsInstanceOfType(restoredModule.GetSourceModule(0), source0.GetType());
+            Assert.IsInstanceOfType(restoredModule.GetSourceModule(1), source1.GetType());
+        }
+    }
+}
--- SharpNoise.Serialization/XmlSerializer.cs
+++ SharpNoise.Serialization/XmlSerializer.cs
@@ -1,8 +1,9 @@
-﻿using SharpNoise.Modules;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Reflection;
+using System.Linq;
+using System.Xml.Linq;
+using Module = SharpNoise.Modules.Module;
 
 namespace SharpNoise.Serialization
 {
@@ -15,28 +16,123 @@ namespace SharpNoise.Serialization
     /// </remarks>
     public class XmlSerializer : Serializer
     {
-        Dictionary<string, string> ReadProperties(SharpNoise.Modules.Module module)
-        {
-            var dict = new Dictionary<string, string>();
 
-            var properties = module.GetType().GetProperties(BindingFlags.Instance | BindingFlags.Public);
-            foreach (var property in properties)
+        #region Save Helpers
+
+        IEnumerable<XElement> CreateModuleElements(IEnumerable<Module> modules, Dictionary<Module, string> idMapping)
+        {
+            var moduleElements = new List<XElement>();
+
+            foreach (var module in modules)
             {
-                var value = property.GetValue(module);
-                dict.Add(property.Name, value.ToString());
+                var propertyMapping = ReadProperties(module);
+                var element = new XElement("Module",
+                    new XAttribute("id", idMapping[module]),
+                    new XAttribute("type", module.GetType().Name),
+                    from property in propertyMapping
+                    select new XElement("Property",
+                        new XAttribute("name", property.Key),
+                        new XAttribute("value", property.Value)));
+                moduleElements.Add(element);
             }
 
-            return dict;
+            return moduleElements;
         }
 
-        protected override void Serialize(Stream targetStream, SharpNoise.Modules.Module module)
+        IEnumerable<XElement> CreateConnectionElements(IEnumerable<Module> modules, Dictionary<Module, string> idMapping)
+        {
+            var connections = new List<XElement>();
+
+            foreach (var module in modules)
+            {
+                for (int i = 0; i < module.SourceModuleCount; ++i)
+                {
+                    var source = module.GetSourceModule(i);
+                    var element = new XElement("Connection",
+                        new XAttribute("for", idMapping[module]),
+                        new XAttribute("source", idMapping[source]),
+                        new XAttribute("slot", i));
+                    connections.Add(element);
+                }
+            }
+
+            return connections;
+        }
+
+        XDocument CreateModuleGraphDocument(IEnumerable<XElement> moduleElements, IEnumerable<XElement> connectionElements, string rootID)
+        {
+            var document = new XDocument(
+                new XDeclaration("1.0", "utf-8", null),
+                new XElement("ModuleGraph",
+                    new XAttribute("root", rootID),
+                    moduleElements,
+                    connectionElements));
+            return document;
+        }
+
+        #endregion
+
+        #region Restore Helpers
+
+        XDocument ReadModuleGraphDocument(Stream source)
+        {
+            var doc = XDocument.Load(source);
+            return doc;
+        }
+
+        Dictionary<string, Module> RestoreModules(IEnumerable<XElement> moduleElements)
         {
             throw new NotImplementedException();
         }
 
-        protected override SharpNoise.Modules.Module Deserialize(Stream sourceStream)
+        void SetProperties(Module instance, Dictionary<string, string> propertyMapping)
         {
             throw new NotImplementedException();
+        }
+
+        void SetSources(IEnumerable<XElement> connectionElements, Dictionary<string, Module> modules)
+        {
+            foreach (var connection in connectionElements)
+            {
+                var moduleID = connection.Attribute("for").Value;
+                var sourceModuleID = connection.Attribute("source").Value;
+                var slot = int.Parse(connection.Attribute("slot").Value);
+
+                var module = modules[moduleID];
+                var source = modules[sourceModuleID];
+
+                module.SetSourceModule(slot, source);
+            }
+        }
+
+        #endregion
+
+        protected override void Serialize(Stream targetStream, Module module)
+        {
+            var modules = GetModulesRecursive(module);
+            var mapping = CreateIDMapping(modules);
+
+            var moduleElements = CreateModuleElements(modules, mapping);
+            var connectionElements = CreateConnectionElements(modules, mapping);
+            var rootID = mapping[module];
+
+            var document = CreateModuleGraphDocument(moduleElements, connectionElements, rootID);
+            document.Save(targetStream);
+        }
+
+        protected override Module Deserialize(Stream sourceStream)
+        {
+            var document = ReadModuleGraphDocument(sourceStream);
+
+            var moduleElements = document.Root.Elements("Module");
+            var connectionElements = document.Root.Elements("Connection");
+            var rootElement = document.Root.Element("Root");
+
+            var moduleMapping = RestoreModules(moduleElements);
+            SetSources(connectionElements, moduleMapping);
+
+            var root = moduleMapping[rootElement.Value];
+            return root;
         }
     }
 }
