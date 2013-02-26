f156a74b139314f24d56d26251a5b45a36b05815
Raffael Thome <thome.raffael@gmail.com>
2013-02-26 15:00:42 +0100

    Add skeleton class for 2d heightmaps

---
+++ SharpNoise.Utilities/Maps/Heightmap.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpNoise.Utilities.Maps
+{
+    /// <summary>
+    /// A "lazy" heightmap of potentially infinite size
+    /// </summary>
+    /// <remarks>
+    /// Chunks of data will be generated when they are first accessed, instead of when the Heightmap is created.
+    /// </remarks>
+    public class Heightmap
+    {
+    }
+}
