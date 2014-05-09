630649ed33d95a9eca98b4fe7a3366443e7fa9de
Raffael Thome <thome.raffael@gmail.com>
2014-05-10 00:03:57 +0200

    IMplement the plains terrain module group

--- ComplexPlanetExample/PlanetGenerator.cs
+++ ComplexPlanetExample/PlanetGenerator.cs
@@ -611,7 +611,74 @@ namespace ComplexPlanetExample
 
         Module CreatePlainsTerrain()
         {
-            throw new NotImplementedException();
+            // Generates the plains terrain.
+            //
+            // Because this subgroup will eventually be flattened considerably, the
+            // types and combinations of noise modules that generate the plains are not
+            // really that important; they only need to "look" interesting.
+            //
+            // -1.0 represents the lowest elevations and +1.0 represents the highest
+            // elevations.
+            //
+            // [Plains-terrain group]: Caches the output value from the rescaled-
+            // plains-basis module.  This is the output value for the entire plains-
+            // terrain group.
+            var plainsTerrain = new Cache
+            {
+                // [Rescaled-plains-basis module]: This scale/bias module maps the output
+                // value that ranges from 0.0 to 1.0 back to a value that ranges from
+                // -1.0 to +1.0.
+                Source0 = new ScaleBias
+                {
+                    Scale = 2,
+                    Bias = -1,
+                    Source0 = new Multiply
+                    {
+                        // [Positive-plains-basis-0 module]: This scale/bias module makes the
+                        // output value from the plains-basis-0 module positive since this output
+                        // value will be multiplied together with the positive-plains-basis-1
+                        // module.
+                        Source0 = new ScaleBias
+                        {
+                            Scale = 0.5,
+                            Bias = 0.5,
+                            // [Plains-basis-0 module]: This billow-noise module, along with the
+                            // plains-basis-1 module, produces the plains.
+                            Source0 = new Billow
+                            {
+                                Seed = Settings.Seed + 70,
+                                Frequency = 1097.5,
+                                Persistence = 0.5,
+                                Lacunarity = Settings.PlainsLacunarity,
+                                OctaveCount = 8,
+                                Quality = NoiseQuality.Best,
+                            },
+                        },
+                        // [Positive-plains-basis-1 module]: This scale/bias module makes the
+                        // output value from the plains-basis-1 module positive since this output
+                        // value will be multiplied together with the positive-plains-basis-0
+                        // module.
+                        Source1 = new ScaleBias
+                        {
+                            Scale = 0.5,
+                            Bias = 0.5,
+                            // [Plains-basis-1 module]: This billow-noise module, along with the
+                            // plains-basis-2 module, produces the plains.
+                            Source0 = new Billow
+                            {
+                                Seed = Settings.Seed + 71,
+                                Frequency = 1319.5,
+                                Persistence = 0.5,
+                                Lacunarity = Settings.PlainsLacunarity,
+                                OctaveCount = 8,
+                                Quality = NoiseQuality.Best,
+                            },
+                        },
+                    },
+                },
+            };
+
+            return plainsTerrain;
         }
 
         Module CreateBadlandsTerrain()
