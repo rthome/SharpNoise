19f9f654aef152082829a37097abddc773479d53
Raffael Thome <thome.raffael@gmail.com>
2014-05-09 14:26:02 +0200

    Add methods to Imagem to serialize and save in one step

--- SharpNoise.Utilities/Imaging/Image.cs
+++ SharpNoise.Utilities/Imaging/Image.cs
@@ -1,4 +1,7 @@
-﻿namespace SharpNoise.Utilities.Imaging
+﻿using System;
+using System.Drawing.Imaging;
+using System.IO;
+namespace SharpNoise.Utilities.Imaging
 {
     /// <summary>
     /// Implements an image, a 2-dimensional array of color values.
@@ -18,11 +21,11 @@
     {
         public override int UsedMemory
         {
-            get 
+            get
             {
                 unsafe
                 {
-                    return values.Length * sizeof(Color); 
+                    return values.Length * sizeof(Color);
                 }
             }
         }
@@ -68,6 +71,54 @@
                 }
             }
             return bitmap;
+        }
+
+        /// <summary>
+        /// Convert the image to a System.Drawing.Bitmap and save it to a stream
+        /// </summary>
+        /// <param name="destStream">The stream to save the image to</param>
+        /// <param name="imageFormat">The image format to use</param>
+        /// <param name="leaveOpen">
+        /// If true, <paramref name="destStream"/> will be left open after saving the image;
+        /// otherwise, the stream will be closed.
+        /// </param>
+        public void SaveGdiBitmap(Stream destStream, ImageFormat imageFormat, bool leaveOpen = true)
+        {
+            if (destStream == null)
+                throw new ArgumentNullException("destStream");
+            if (!destStream.CanWrite)
+                throw new ArgumentException("Given stream cannot be written to.", "destStream");
+            if (imageFormat == null)
+                throw new ArgumentNullException("imageFormat");
+
+            var bitmap = ToGdiBitmap();
+            bitmap.Save(destStream, imageFormat);
+
+            if (!leaveOpen)
+                destStream.Close();
+        }
+
+        /// <summary>
+        /// Convert the image to a System.Drawing.Bitmap and save it to a file
+        /// </summary>
+        /// <param name="filename">The file to save the image to</param>
+        /// <param name="imageFormat">The ImageFormat to use</param>
+        public void SaveGdiBitmap(string filename, ImageFormat imageFormat)
+        {
+            if (filename == null)
+                throw new ArgumentNullException("filename");
+            if (imageFormat == null)
+                throw new ArgumentNullException("imageFormat");
+
+            try
+            {
+                using (var stream = File.OpenWrite(filename))
+                    SaveGdiBitmap(stream, imageFormat);
+            }
+            catch (IOException exc)
+            {
+                throw new IOException(String.Format("Cannot write to given file '{0}'", filename), exc);
+            }
         }
     }
 }
