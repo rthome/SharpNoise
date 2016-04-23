e7c9deea6927da52368819da29be69a6b4ae9588
InfiniteProductions <justice8@bbox.fr>
2016-04-23 11:50:17 +0200

    Add Discrete modifier module and its unit tests

---
+++ SharpNoise.Tests/ModuleTests/DiscreteTests.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SharpNoise.Modules;
+
+namespace SharpNoise.Tests.ModuleTests
+{
+    [TestClass]
+    public class DiscreteTests
+    {
+        Random random;
+
+        const Byte VALUES = 4;
+
+        public void DiscreteMainTestCode(Module source, Byte Count = VALUES)
+        {
+            Assert.IsNotNull(source);
+
+            Discrete module = new Discrete() { Source0 = source, Values = Count };
+
+            random = new Random((int)DateTime.Now.Ticks & (0x0000FFFF));
+
+            double sourceValue, checkvalue;
+            double x, y, z;
+
+            for (Byte count = 0; count < 50; count++)
+            {
+                x = random.NextDouble();
+                y = random.NextDouble();
+                z = random.NextDouble();
+
+                sourceValue = source.GetValue(x, y, z);
+
+                checkvalue = 0;
+
+                for (Byte value = 0; value < Count; value++)
+                {
+                    if (sourceValue >= value * 1.0 / Count && sourceValue < (value + 1) * 1.0 / Count)
+                    {
+                        checkvalue = value * 1.0 / Count;
+                    }
+                }
+
+                if (checkvalue != 0)
+                {
+                    Assert.AreEqual(checkvalue, module.GetValue(x, y, z));
+                }
+                else
+                {
+                    Assert.AreEqual(0, module.GetValue(x, y, z));
+                }
+
+            }
+        }
+
+
+        [TestMethod]
+        public void DiscretePerlinTest()
+        {
+            Perlin mperlin = new Perlin();
+            
+            DiscreteMainTestCode(mperlin, 3);
+        }
+
+        [TestMethod]
+        public void DiscreteBillowTest()
+        {
+            Billow mbillow = new Billow();
+            mbillow.Frequency = 0.5;
+            mbillow.Persistence = 0.8;
+
+            DiscreteMainTestCode(mbillow, 5);
+        }
+
+        [TestMethod]
+        public void DiscreteVoronoiTest()
+        {
+            Voronoi mvoronoi = new Voronoi();
+
+            ScaleBias mscabi = new ScaleBias();
+            mscabi.Source0 = mvoronoi;
+            mscabi.Bias = 0.6;
+            mscabi.Scale = 0.5;
+
+            DiscreteMainTestCode(mscabi, 4);
+        }
+    }
+}
---
+++ SharpNoise/Modules/Discrete.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace SharpNoise.Modules
+{
+    /// <summary>
+    /// This module take a value from a source module and from a given input "slice", return a discrete value, so its name, in the interval [i/slice, (i + 1)/slice]
+    /// For example, if slice count is set to 4, the Perlin source module values are converted into 0, 1/4, 2/4, 3/4 and 1 (4/4) ONLY, creating a sharp map which can be use
+    /// as filter for a select module or as-is for a tile map based game.
+    /// </summary>
+    /// <remarks>
+    /// This noise module requires one source module.
+    /// </remarks>
+    [Serializable]
+    public class Discrete : Module
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public Discrete()
+            : base(1)
+        {
+
+        }
+
+        /// <summary>
+        /// Gets or sets the number of maximum values to get from
+        /// input value.
+        /// </summary>
+        public UInt16 Values { get; set; }
+
+        /// <summary>
+        /// Gets or sets the first, and only, source module
+        /// </summary>
+        public Module Source0
+        {
+            get { return SourceModules[0]; }
+            set { SourceModules[0] = value; }
+        }
+
+        /// <summary>
+        /// See the documentation on the base class.
+        /// <seealso cref="Module"/>
+        /// </summary>
+        /// <param name="x">X coordinate</param>
+        /// <param name="y">Y coordinate</param>
+        /// <param name="z">Z coordinate</param>
+        /// <returns>Returns the computed value</returns>
+        public override double GetValue(double x, double y, double z)
+        {
+            double srcModValue = SourceModules[0].GetValue(x, y, z);
+
+            if (srcModValue <= 0)
+            {
+                return 0.0;
+            }
+
+            for (uint i = 0; i < Values; i++)
+            {
+                if (srcModValue >= i * 1.0 / Values && srcModValue < (i + 1) * 1.0 / Values)
+                {
+                    return i * 1.0/Values;
+                }
+            }
+
+            return 0.0;
+        }
+    }
+}
