d593833435fbfd85f587d470868ecb4dd6472aba
Raffael Thome <mail@rmt.name>
2016-04-26 20:24:17 +0200

    Reimplement what little tests for Utilities classes there are

---
+++ SharpNoise.Tests/Utilities/ColorTests.cs
@@ -0,0 +1,65 @@
+﻿using SharpNoise.Utilities.Imaging;
+using Xunit;
+
+namespace SharpNoise.Tests.Utilities
+{
+    public class ColorTests
+    {
+        [Fact]
+        public void Color_CopyConstructor_Test()
+        {
+            var expected = new Color(120, 5, 80, 255);
+            var copy = new Color(expected);
+
+            Assert.Equal(expected, copy);
+        }
+
+        [Fact]
+        public void Color_LinearInterp_Middle_Test()
+        {
+            var color1 = new Color(100, 100, 100, 100);
+            var color2 = new Color(0, 0, 0, 0);
+
+            var expected = new Color(50, 50, 50, 50);
+            var actual = Color.LinearInterpColor(color1, color2, 0.5f);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Color_LinearInterp_Upper_Test()
+        {
+            var color1 = new Color(100, 100, 100, 100);
+            var color2 = new Color(0, 0, 0, 0);
+
+            var expected = new Color(75, 75, 75, 75);
+            var actual = Color.LinearInterpColor(color1, color2, 0.25f);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Color_LinearInterp_Middle2_Test()
+        {
+            var color1 = new Color(100, 100, 100, 100);
+            var color2 = new Color(200, 200, 200, 200);
+
+            var expected = new Color(150, 150, 150, 150);
+            var actual = Color.LinearInterpColor(color1, color2, 0.5f);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Color_LinearInterp_Same_Test()
+        {
+            var color1 = new Color(100, 100, 100, 100);
+            var color2 = new Color(color1);
+
+            var expected = color1;
+            var actual = Color.LinearInterpColor(color1, color2, 0.5f);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ SharpNoise.Tests/Utilities/GradientColorTests.cs
@@ -0,0 +1,53 @@
+﻿using SharpNoise.Utilities.Imaging;
+using System;
+using Xunit;
+
+namespace SharpNoise.Tests.Utilities
+{
+    public class GradientColorTests
+    {
+        readonly GradientColor gradient;
+
+        public GradientColorTests()
+        {
+            gradient = new GradientColor();
+            gradient.AddGradientPoint(0, new Color(0, 0, 0, byte.MaxValue));
+            gradient.AddGradientPoint(1, new Color(byte.MaxValue, 0, 0, byte.MaxValue));
+        }
+
+        [Fact]
+        public void InsertPointTest()
+        {
+            var expected = 2;
+            var actual = gradient.PointCount;
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void InsertSamePointTest()
+        {
+            Assert.Throws<ArgumentException>(() => gradient.AddGradientPoint(1, new Color()));
+        }
+
+        [Theory]
+        [InlineData(1.0, byte.MaxValue, 0, 0, byte.MaxValue)]
+        [InlineData(0.5, byte.MaxValue / 2, 0, 0, byte.MaxValue)]
+        [InlineData(0.0, 0, 0, 0, byte.MaxValue)]
+        public void GetColorTest(double alpha, byte r, byte g, byte b, byte a)
+        {
+            var color = gradient.GetColor(alpha);
+            var expectedColor = new Color(r, g, b, a);
+
+            Assert.Equal(expectedColor, color);
+        }
+
+        [Fact]
+        public void ClearPointsTest()
+        {
+            gradient.ClearGradientPoints();
+
+            Assert.Equal(0, gradient.PointCount);
+        }
+    }
+}
