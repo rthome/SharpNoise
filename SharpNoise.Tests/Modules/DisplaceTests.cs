749342ac6b94f67ae2b582f71aefeea8fe92eb78
Raffael Thome <mail@rmt.name>
2016-05-01 12:44:27 +0200

    More test classes

---
+++ SharpNoise.Tests/Modules/DisplaceTests.cs
@@ -0,0 +1,9 @@
+﻿namespace SharpNoise.Tests.Modules
+{
+    /// <summary>
+    /// Tests for the <see cref="Displace"/> module
+    /// </summary>
+    public class DisplaceTests
+    {
+    }
+}
---
+++ SharpNoise.Tests/Modules/ExponentTests.cs
@@ -0,0 +1,9 @@
+﻿namespace SharpNoise.Tests.Modules
+{
+    /// <summary>
+    /// Tests for the <see cref="Exponent"/> module
+    /// </summary>
+    public class ExponentTests
+    {
+    }
+}
