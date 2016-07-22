943d4fb3b5d2ab7e39f6dc9cf97a2bad0ba2be55
Raffael Thome <mail@rmt.name>
2016-07-23 00:36:19 +0200

    Increment versions to 0.11.0

--- SharpNoise.Utilities/Properties/AssemblyInfo.cs
+++ SharpNoise.Utilities/Properties/AssemblyInfo.cs
@@ -12,5 +12,5 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("2fd0b3f2-6267-4382-b794-d142b2a604ed")]
 
-[assembly: AssemblyVersion("0.10.0.*")]
-[assembly: AssemblyFileVersion("0.10.0")]
+[assembly: AssemblyVersion("0.11.0.*")]
+[assembly: AssemblyFileVersion("0.11.0")]
